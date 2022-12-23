cfb72b66b4ccef2a08116dd7248f4f7fade87d1a
AbdulMoizAli <moizali_@outlook.com>
2022-12-24 01:01:05 +0500

    configure tank level

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,12 @@
+import { httpGet } from '../modules/httpClient.js';
+import configureTank from '../modules/tank.js';
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {}
+async function onDeviceReady() {
+    const latestState = await httpGet(
+        'rest/status-logs?h={"$aggregate":["MAX:createdOn"]}'
+    );
+
+    configureTank(latestState);
+}
